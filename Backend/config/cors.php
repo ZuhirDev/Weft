8f1b9f90d474c043df665cd02d4f3a3fb0d50a44
Ubuntu <ubuntu@ip-172-31-24-14.ec2.internal>
2025-06-06 12:53:13 +0000

    Weft Deploy

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:80', 'http://weft.atisoluciones.es/', 'https://weft.atisoluciones.es/'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:80', 'http://weft.atisoluciones.es', 'https://weft.atisoluciones.es'],
 
     'allowed_origins_patterns' => [],
 
